a9e6965e19fcc87ad42cfbccd2198d04e789d609
Biljana <sotirovskabiljana@gmail.com>
2020-12-01 18:05:47 +0100

    Refactored feedback form to be created with ng-repeat

--- app/src/controllers/NewFeedbackController.js
+++ app/src/controllers/NewFeedbackController.js
@@ -1,22 +1,56 @@
 reviewApp.controller("NewFeedbackController", ['$scope', '$location', 'ReviewsDataService', '$filter', function NewFeedbackController($scope, $location, ReviewsDataService, $filter) {
-    $scope.title = "Here should be the new feedback form";
     $scope.emojisShown = false;
-    // $scope.review = {};
+    $scope.review = {};
     $scope.selectedStars = 0;
     $scope.emojis = ['&#x1F620;', '&#x1F61E;', '&#x1F610;', '&#x1F60A;', '&#x1F603;'];
     $scope.emoji = '';
     $scope.hoveredId = 0;
+    $scope.fields = [
+        {
+            name: 'name',
+            required: true,
+            type: 'text',
+            placeholder: 'Name',
+            label: 'Your name'
+        },
+        {
+            name: 'email',
+            required: true,
+            type: 'text',
+            placeholder: 'Email address',
+            label: 'Your email address'
+        },
+        {
+            name: 'title',
+            required: true,
+            type: 'text',
+            placeholder: 'Title',
+            label: 'Title of your review'
+        },
+        {
+            name: 'content',
+            required: true,
+            type: 'textarea',
+            placeholder: 'Please try to be nice and constructive',
+            label: 'Your review'
+        }
+    ];
 
     $scope.submitForm = function (review) {
         if ($scope.selectedStars === 0) {
             alert("Please give us at least one star...");
             return;
         }
-        // review.date = $filter('date')(new Date(), 'dd-MM-yyyy');
-        review.date = new Date();
-        review.rating = $scope.selectedStars;
-        review.votes = 0;
-        ReviewsDataService.submitReview(review)
+        let newReview = {};
+        newReview.user = {};
+        newReview.user.name = review.name;
+        newReview.user.email = review.email;
+        newReview.title = review.title;
+        newReview.content = review.content;
+        newReview.date = new Date();
+        newReview.rating = $scope.selectedStars;
+        newReview.votes = 0;
+        ReviewsDataService.submitReview(newReview)
             .then(res => {
                 if(res.status === 200) {
                     $location.url('/reviews');
@@ -26,8 +60,6 @@ reviewApp.controller("NewFeedbackController", ['$scope', '$location', 'ReviewsDa
                 alert("Your review can not be submitted at the moment. Please try again later");
                 console.error(err);
             });
-        // console.log("Form submitted: ", review);
-        // $location.url('/reviews');
     };
     $scope.showEmojis = function () {
         $scope.emoji = $scope.emojis[$scope.hoveredId - 1];
@@ -43,7 +75,15 @@ reviewApp.controller("NewFeedbackController", ['$scope', '$location', 'ReviewsDa
     return {
         restrict: 'A',
         link: function (scope, element) {
-            $('.emojis').css({'margin-left': '50px'});
+            let offsetLeft = $('.starRatingWidget').offset();
+            let starsWidth = $('.glyphicon').css('width');
+            let emojiMarginLeft = offsetLeft.left + 'px';
+            $('.emojis').css({'margin-left': emojiMarginLeft});
+            // console.log(parseInt($('.container').css('width')) / 4 + 'px');
+            let feedbackFormMarginLeft = parseInt($('.container').css('width')) / 4;
+            $('.feedbackFormWidget').css({left: feedbackFormMarginLeft, position: 'relative'});
+            let buttonLeftPosition = parseInt($('.feedbackFormWidget').css('width')) / 2;
+            $('.feedbackFormWidget button').css({left: buttonLeftPosition, position: 'relative'});
         }
     };
 });
